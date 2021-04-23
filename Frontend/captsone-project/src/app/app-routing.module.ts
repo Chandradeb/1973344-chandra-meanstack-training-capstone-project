70557e450548490bd3ac608a5a0edaa59a784298
Chandra Deb <ananda.deb12@gmail.com>
2021-04-22 23:12:34 -0400

    Revert "Main page created"
    This reverts commit 2dc2f2878d62d3e68159b5d1cde66cb53cf02575.

--- Frontend/captsone-project/src/app/app-routing.module.ts
+++ Frontend/captsone-project/src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminMainComponent } from './admin-main/admin-main.component';
-import { EmployeeMainComponent } from './employee-main/employee-main.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { UserMainComponent } from './user-main/user-main.component';
 
-const routes: Routes = [
-  {path: "main-page", component: MainPageComponent},
-  {path: "user-main", component: UserMainComponent},
-  {path: "employee-main", component: EmployeeMainComponent},
-  {path: "admin-main", component: AdminMainComponent},
-  {path:"",redirectTo:"\main-page",pathMatch:"full"}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
