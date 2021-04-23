2dc2f2878d62d3e68159b5d1cde66cb53cf02575
Chandra Deb <ananda.deb12@gmail.com>
2021-04-22 23:03:28 -0400

    Main page created

--- Frontend/captsone-project/src/app/app.module.ts
+++ Frontend/captsone-project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserMainComponent } from './user-main/user-main.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { EmployeeMainComponent } from './employee-main/employee-main.component';
+import { AdminMainComponent } from './admin-main/admin-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserMainComponent,
+    MainPageComponent,
+    EmployeeMainComponent,
+    AdminMainComponent
   ],
   imports: [
     BrowserModule,
