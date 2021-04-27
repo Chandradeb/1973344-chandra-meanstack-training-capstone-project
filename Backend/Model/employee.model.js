d7d2888dbe8a04dbf9660cf45b2d67b3e09b84c0
Chandra Deb <ananda.deb12@gmail.com>
2021-04-27 15:27:38 -0400

    Update employee.model.js

--- Backend/Model/employee.model.js
+++ Backend/Model/employee.model.js
@@ -1,21 +1,3 @@
-let mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-const Schema = mongoose.Schema;      // creating reference. 
-
-let EmployeeSchema = new Schema({
-    _id:Number,
-    fname:String,
-    lname:String,
-    emailid:String,
-    password:String
-})
-
-
-
-
-let EmployeeModel = mongoose.model("",EmployeeSchema,"Employee");
-
-module.exports = EmployeeModel;
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 
