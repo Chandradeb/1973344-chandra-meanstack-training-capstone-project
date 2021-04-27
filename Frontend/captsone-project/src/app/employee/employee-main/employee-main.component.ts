7b361a0b09e1b7bfd8c6a85b6ae1a7c8b01a58ed
Chandra Deb <ananda.deb12@gmail.com>
2021-04-27 08:11:13 -0400

    Update employee-main.component.ts

--- Frontend/captsone-project/src/app/employee/employee-main/employee-main.component.ts
+++ Frontend/captsone-project/src/app/employee/employee-main/employee-main.component.ts
@@ -34,16 +34,26 @@ export class EmployeeMainComponent implements OnInit {
     this.empService.signIn(employee).subscribe(data =>{
       this.signInData=data;
       if(this.signInData.success){
+        //print successful sign in messsage
         this.toastr.success('Successfully Signed In', 'Success', {
           timeOut: 2000,
         });
+
+        //token for employee login
         sessionStorage.setItem('employeeSignedIn', 'true');
+
+        //user information for using to edit profile
         sessionStorage.setItem('employeeDetails', JSON.stringify(this.signInData.employee));
+
+        //navigate tp employee dashboard
         this.router.navigate(['employee-dashboard']);
       }else{
+        //print error message for wrong credentials
         this.toastr.error(this.signInData.msg, 'Error', {
           timeOut: 2000,
         });
+
+        //route back to employee-main
         this.router.navigate(['employee-main']);
       }
     });
